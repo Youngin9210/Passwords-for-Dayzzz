8dd8c5ef7dce30858b1e465e091d54bd3e1df86b
Kyle Young <kyleyoung.9210@gmail.com>
2021-02-08 22:39:53 -0500

    Moved criteria outside of writePassword function.  Added character variables.

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var upperCase = lowerCase.toUpperCase();
+var numbers = Math.floor(Math.random() * 10);
+var special =  '`~!@#$%^&*()_-+=,.<>/?;:';
+
+var generatePassword = function () {
+  // declaring length prompt and converting to a number
+  var length = Number(prompt('Choose a desired character length between 8 and 128?'));
+
+  //  if length is bewteen 8 and 128,
+  if (length > 7 && length < 129) {
+    // then declare character type confirm variables
+    var lowerChar = confirm('Would you like to include lowercase?');
+    var upperChar = confirm('Would you like to include uppercase?');
+    var numberChar = confirm('Would you like to include numbers?');
+    var specialChar = confirm('Would you like to include special characters?');
+
+      // if no character type selected
+    if (!lowerChar && !upperChar && !numbersChar && !specialChar) {
+      // then alert to restart and choose one
+      alert('Please select at least one character type. Press "Generate Password" to restart.')
+    }
+  } else {
+    // then alert restart and choose a number between 8 and 128
+    alert('Please choose a length bewteen 8 and 128.  Press "Generate Password" to restart.');
+  }
+}
 
 
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-  
-  var chars = function(){
-    var length = Number(prompt('Choose a desired character length between 8 and 128?'));
-    if (length > 7 && length < 129) {
-      return;
-    } else {
-      chars();
-    }
-  }
-  chars();
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  
-  var lowercase = confirm('Would you like to include lowercase?');
-  var uppercase = confirm('Would you like to include uppercase?');
-  var numbers = confirm('Would you like to include numbers?');
-  var special = confirm('Would you like to include special characters?');
-
-  // passwordText.value = password;
+  passwordText.value = password;
 
 }
 
