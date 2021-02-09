4eb5ddaf37867710343006861abb83276e941b2b
Kyle Young <kyleyoung.9210@gmail.com>
2021-02-09 17:14:21 -0500

    add char variations for desired char types in pass

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// creating character variables
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-var upperCase = lowerCase.toUpperCase();
-var numbers = Math.floor(Math.random() * 10);
-var special =  '`~!@#$%^&*()_-+=,.<>/?;:';
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';;
+var numbers = '0123456789';
+var special = '`~!@#$%^&*()_-+=,.<>/?;:';
+
+var randomPass = "";
 
 var generatePassword = function () {
   // declaring length prompt and converting to a number
-  var length = Number(prompt('Choose a desired character length between 8 and 128?'));
+  var charLength = Number(prompt('Choose a desired character length between 8 and 128?'));
 
-  //  if length is bewteen 8 and 128,
-  if (length > 7 && length < 129) {
-    // then declare character type confirm variables
+  //  if charLength is bewteen 8 and 128,
+  if (charLength > 7 && charLength < 129) {
+
+    console.log(charLength);
+
+    // confrim character type to be used in password
     var lowerChar = confirm('Would you like to include lowercase?');
     var upperChar = confirm('Would you like to include uppercase?');
     var numberChar = confirm('Would you like to include numbers?');
     var specialChar = confirm('Would you like to include special characters?');
 
-      // if no character type selected
-    if (!lowerChar && !upperChar && !numbersChar && !specialChar) {
+    // if no character type selected
+    if (!lowerChar && !upperChar && !numberChar && !specialChar) {
       // then alert to restart and choose one
       alert('Please select at least one character type. Press "Generate Password" to restart.')
     }
+    
+    // setting character variations to be used for password
+    var charA = lowerCase + upperCase + numbers + special; // done
+    var charB = lowerCase + upperCase + numbers; // done
+    var charC = lowerCase + upperCase + special; // done
+    var charD = lowerCase + special + numbers;
+    var charE = special + upperCase + numbers;
+    var charF = lowerCase + upperCase;
+    var charG = lowerCase + numbers;
+    var charH = lowerCase + special;
+    var charI = upperCase + numbers;
+    var charJ = upperCase + special;
+    var charK = numbers + special;
+    var charL = lowerCase;
+    var charM = upperCase;
+    var charN = numbers;
+    var charO = special;
+
+
   } else {
     // then alert restart and choose a number between 8 and 128
     alert('Please choose a length bewteen 8 and 128.  Press "Generate Password" to restart.');
   }
+  
+
 }
 
 
