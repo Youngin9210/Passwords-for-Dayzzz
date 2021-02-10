6b0b466b9039e05e0a1a109618d2fb611e75c631
Kyle Young <kyleyoung.9210@gmail.com>
2021-02-10 14:52:17 -0500

    updated special characters. Added in to check if charLength is an integer in if statement.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';;
 var numbers = '0123456789';
-var special = '`~!@#$%^&*()_-+=,.<>/?;:';
+var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
 var randomPass = "";
 
@@ -13,8 +13,8 @@ var generatePassword = function () {
   // declaring length prompt and converting to a number
   var charLength = Number(prompt('Choose a desired character length between 8 and 128?'));
 
-  //  if charLength is bewteen 8 and 128,
-  if (charLength > 7 && charLength < 129) {
+  //  if charLength is bewteen 8 and 128 AND the number is a whole integer,
+  if ((charLength > 7 && charLength < 129) && Number.isInteger(charLength)) {
 
 
     // confrim character type to be used in password
@@ -30,17 +30,17 @@ var generatePassword = function () {
     }
     
     // setting character variations to be used for password
-    var charA = lowerCase + upperCase + numbers + special; // if done
-    var charB = lowerCase + upperCase + numbers; // if done
-    var charC = lowerCase + upperCase + special; // if done
-    var charD = lowerCase + special + numbers; // if done
-    var charE = special + upperCase + numbers; // if done
-    var charF = lowerCase + upperCase; // if done
-    var charG = lowerCase + numbers; // if done
-    var charH = lowerCase + special; // if done
-    var charI = upperCase + numbers; // if done
-    var charJ = upperCase + special; // if done
-    var charK = numbers + special; // if done
+    var charA = lowerCase + upperCase + numbers + special;
+    var charB = lowerCase + upperCase + numbers;
+    var charC = lowerCase + upperCase + special;
+    var charD = lowerCase + special + numbers;
+    var charE = special + upperCase + numbers;
+    var charF = lowerCase + upperCase;
+    var charG = lowerCase + numbers;
+    var charH = lowerCase + special;
+    var charI = upperCase + numbers;
+    var charJ = upperCase + special;
+    var charK = numbers + special;
 
     // if statements for all variations to return the password
     if (lowerChar && upperChar && numberChar && specialChar) {
@@ -127,7 +127,7 @@ var generatePassword = function () {
 
   } else {
     // then alert restart and choose a number between 8 and 128
-    alert('Please choose a length bewteen 8 and 128.  Press "Generate Password" to restart.');
+    alert('Please choose a length bewteen 8 and 128.  Length must be a WHOLE integer.  Press "Generate Password" to restart.');
   }
 
 }
